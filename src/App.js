3de1e0c4e9f89278e5b3f300bb0a9bc69f0a6abf
mortenjogi <morten.jogi@gmail.com>
2021-03-02 17:16:35 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react"
 import './App.css';
 
+import * as objectData from "./object-data.json"
+
+import { MapContainer, TileLayer } from 'react-leaflet'
+import MarkerComponent from "./MapComponent";
+
 function App() {
+
+  const [seconds, setSeconds] = useState(0);
+  const [arrayElement, setArrayElement] = useState(0);
+
+  useEffect(() => {
+    if (arrayElement === (objectData.objects[0].coordinates.length - 1)) {
+      setArrayElement(0)
+    } else {
+      setArrayElement(arrayElement + 1)
+    }
+    // eslint-disable-next-line
+  }, [seconds])
+
+  useEffect(() => {
+    console.log(arrayElement)
+  }, [arrayElement])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds(seconds => seconds + 3);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>
+        <MapContainer center={[59.397906, 24.657638]} zoom={12} >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {objectData.objects.map(object => 
+            <MarkerComponent
+              key={object.id}
+              object={object} 
+              arrayElement={arrayElement}
+            />
+          )}
+        </MapContainer>
+      </div>
+    </>
+    );
+
+
 }
 
 export default App;
---
+++ src/MapComponent.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Marker, Popup } from 'react-leaflet'
+
+export default function markerComponent({ arrayElement, object }) {
+  console.log(object)
+
+    return (
+        <>
+        <Marker 
+          key={object.id} 
+          position={[object.coordinates[arrayElement][0], object.coordinates[arrayElement][1]]}
+          >
+          <Popup>
+            <div>
+              <h2>{object.type}</h2>
+              <p>Coordinates: {object.coordinates[0][0]}, {object.coordinates[0][1]}</p>
+            </div>
+          </Popup>
+        </Marker>
+      </>
+)}
